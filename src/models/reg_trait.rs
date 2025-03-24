63e1e39e8cd2f3a7a72db1157501329fffe62232
Jarbas Santos <jarbas.santos.0409@gmail.com>
2025-03-24 01:23:45 -0300

    docs: :memo: Add some useful code comments

--- src/models/reg_trait.rs
+++ src/models/reg_trait.rs
@@ -1,9 +1,19 @@
 pub trait Reg: std::fmt::Debug {
+    /*
+     * Create a new register from a Vector of &str
+     * 
+     * @param fields: Vec<&str> - The vector of strings
+     * @return Self - The new register
+     */
     fn new(fields: Vec<&str>) -> Self
     where
         Self: Sized;
 
-    // Convert the register to a line of the file
+    /*
+     * Convert the register to a line of the file
+     * 
+     * @return String - The line as a string
+     */
     #[allow(dead_code)]
     fn to_line(&self) -> String {
         String::new()
